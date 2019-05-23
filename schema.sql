c04fe27722270d227c5a206c8573b81e475d5519
Ben Cason <bencason9@gmail.com>
2019-05-23 18:58:47 -0400

    final code

--- schema.sql
+++ schema.sql
@@ -4,11 +4,12 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-    item_id INT NOT NULL PRIMARY KEY,
+    item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR (100) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     quantity INT(10) NOT NULL
+    PRIMARY KEY(item_id)
 )
 
 SELECT * from products;
